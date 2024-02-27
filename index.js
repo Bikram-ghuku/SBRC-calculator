ad1485a2ba9a26dadd73d26bcb3562805b7a2f57
Bikram Ghuku <bikramghuku05@gmail.com>
2024-02-27 18:08:32 +0530

    feat: added calculation ability to calculator

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ window.onload = () => {
     var disp = document.getElementById("display")
     var curr = ""
     var arr = []
+    var all = ""
 
     function process(val){
         if((val >= 0 && val <=9) || val == '.'){
+            all+=val;
             disp.value = ""
             console.log("Number")
             curr+=val;
             disp.value = curr;
         }else if(val == '='){
             console.log(arr)
+            var lsEle = all.charAt(all.length - 1)
+            var res;
+            if(lsEle >= '0' && lsEle <= '9' ){
+                res = all
+            }else{
+                res = all.slice(0, all.length - 1)
+            }
+            console.log(res, math.evaluate(res))
         }else{
+            all+=val;
             arr.push(curr)
             curr=""
             disp.value = ""
